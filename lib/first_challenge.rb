9bdb8a3fec6d23fb740f8d768709887c16d46c83
Ryan M <rmccauleymusic@gmail.com>
2019-11-02 20:24:10 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,8 @@ def first_challenge
          :age => 15,
          :status => "alive"}
 
-       :hero_friends => [ {
+       :hero_friends => [ 
+         {
          :name => "Benvolio",
          :age => "17",
          :attitude => "worried"
@@ -25,7 +26,7 @@ def first_challenge
         :age => "18",
         :attitude => "hot-headed"
       }
-    ] }
+    ] 
 
      :capulet => {
        :patriarch => {
